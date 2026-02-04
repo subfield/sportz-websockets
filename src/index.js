e9a3e1922069683ab794e7e43521991ec08500cd
subfield <tetelainv@gmail.com>
2026-02-04 01:59:35 +0100

    feat: Add database-related dependencies and integrate a new matches router.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import matchRouter from './routes/matches.js';
 
 const app = express();
 const PORT = 8081;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'HELLO WORLD!' });
 });
+
+app.use("/matches", matchRouter);
 
 // Start server
 app.listen(PORT, () => {
